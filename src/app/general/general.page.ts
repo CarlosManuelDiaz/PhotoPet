a29ad65fb50cdcce27a3bcc76af74ee68f3618b6
nuria <nuriamart78@hotmail.com>
2019-03-26 16:24:51 +0100

    Carga de imagenes con ngfor en pagina general

--- src/app/general/general.page.ts
+++ src/app/general/general.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { importType } from '@angular/compiler/src/output/output_ast';
+import { BooleanValueAccessor } from '@ionic/angular';
 
 @Component({
   selector: 'app-general',
@@ -7,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralPage implements OnInit {
 
-  public fotos:Array<string> = []; 
+  public fotos: Array<any> = [{
+    comentario: 'este perro es blanco',
+    foto: '../assets/img/perro.jpg'
+  }, {
+      comentario: 'este perro es blanco',
+      foto: '../assets/img/perro.jpg'
+    }, {
+      comentario: 'este perro es blanco',
+      foto: '../assets/img/perro.jpg'
+    },
+];
+   
 
   constructor() { }
 
