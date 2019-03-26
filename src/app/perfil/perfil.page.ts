956813c28482b2ccb6cafac4b3a0432b192404d1
cdd <cdd@gmail.com>
2019-03-26 13:49:48 +0100

    añadida funcion camara

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-perfil',
@@ -20,9 +21,25 @@ export class PerfilPage implements OnInit {
     {rutaimagen: '../assets/img/6.jpg'},
   ];
 
-  constructor() { }
+  currentImage: Array<string> = [];
+
+  constructor(private camera: Camera) {
+ }
 
   ngOnInit() {
   }
-
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+  this.camera.getPicture(options).then((imageData) => {
+    this.imagenes.push({rutaimagen: 'data:image/jpeg;base64,' + imageData});
+  }, (err) => {
+   // Handle error
+   console.log(this.currentImage);
+  });
+}
 }
