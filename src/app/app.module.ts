b6e2ed235d28f992bbbd352be6804ed8fd3c7dec
cdd <cdd@gmail.com>
2019-03-27 15:55:04 +0100

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,31 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { Camera } from '@ionic-native/camera/ngx';
+import { Chooser } from '@ionic-native/chooser/ngx';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule
+
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    Chooser,
+    ImagePicker,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
